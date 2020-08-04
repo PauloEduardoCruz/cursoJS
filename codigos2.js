c280b1076c61f778473b4622d071c7e856866931
PauloEduardoCruz <pauloeduardo.crp@gmail.com>
2020-08-04 03:47:29 -0300

    velha funcional

--- codigos2.js
+++ codigos2.js
@@ -1,8 +1,209 @@
+document.body.style.background = '#333'
+var x = 0
+var umaVez = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-var bt = document.getElementById('bt')
 
-function add(){
-    bt.innerHTML = "X"
+function velha(){
+    a1.innerHTML = ''
+    a2.innerHTML = ''
+    a3.innerHTML = ''
+    a4.innerHTML = ''
+    a5.innerHTML = '.'
+    a6.innerHTML = ''
+    a7.innerHTML = ''
+    a8.innerHTML = ''
+    a9.innerHTML = ''
+    umaVez = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    x = 0
+    document.body.style.background = '#333'
+}
+
+
+var p1 = document.getElementById('p1')
+var pontosJ1 = 0
+function jogador1(){
+    a1.innerHTML = ''
+    a2.innerHTML = ''
+    a3.innerHTML = ''
+    a4.innerHTML = ''
+    a5.innerHTML = '.'
+    a6.innerHTML = ''
+    a7.innerHTML = ''
+    a8.innerHTML = ''
+    a9.innerHTML = ''
+    umaVez = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    x = 0
+    pontosJ1++
+    p1.innerHTML=pontosJ1
+    document.body.style.background = '#333'
+}
+
+
+var p2 = document.getElementById('p2')
+var pontosJ2 = 0
+function jogador2(){
+    a1.innerHTML = ''
+    a2.innerHTML = ''
+    a3.innerHTML = ''
+    a4.innerHTML = ''
+    a5.innerHTML = '.'
+    a6.innerHTML = ''
+    a7.innerHTML = ''
+    a8.innerHTML = ''
+    a9.innerHTML = ''
+    umaVez = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    x = 0
+    pontosJ2++
+    p2.innerHTML=pontosJ2
+    document.body.style.background = '#333'
+}
+
+
+
+
+var a1 = document.getElementsByClassName('a1')[0]
+function a1f(){
+    if(umaVez[0] == 0){
+        if(x == 0){
+            a1.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a1.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[0]=1
+    }
+}
+
+var a2 = document.getElementsByClassName('a2')[0]
+function a2f(){
+    if(umaVez[1] == 0){
+        if(x == 0){
+            a2.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a2.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[1]=1
+    }
+}
+
+
+var a3 = document.getElementsByClassName('a3')[0]
+function a3f(){
+    if(umaVez[2] == 0){
+        if(x == 0){
+            a3.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a3.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[2]=1
+    }
+}
+
+var a4 = document.getElementsByClassName('a4')[0]
+function a4f(){
+    if(umaVez[3] == 0){
+        if(x == 0){
+            a4.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a4.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[3]=1
+    }
+}
+
+var a5 = document.getElementsByClassName('a5')[0]
+function a5f(){
+    if(umaVez[4] == 0){
+        if(x == 0){
+            a5.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a5.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[4]=1
+    }
+}
+
+var a6 = document.getElementsByClassName('a6')[0]
+function a6f(){
+    if(umaVez[5] == 0){
+        if(x == 0){
+            a6.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a6.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[5]=1
+    }
+}
+
+var a7 = document.getElementsByClassName('a7')[0]
+function a7f(){
+    if(umaVez[6] == 0){
+        if(x == 0){
+            a7.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a7.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[6]=1
+    }
+}
+
+var a8 = document.getElementsByClassName('a8')[0]
+function a8f(){
+    if(umaVez[7] == 0){
+        if(x == 0){
+            a8.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a8.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[7]=1
+    }
+}
+
+var a9 = document.getElementsByClassName('a9')[0]
+function a9f(){
+    if(umaVez[8] == 0){
+        if(x == 0){
+            a9.innerHTML = 'X'
+            x = 1
+            document.body.style.background = 'tomato'
+        } else{
+            a9.innerHTML = 'O'
+            x = 0
+            document.body.style.background = 'teal'
+        }
+        umaVez[8]=1
+    }
 }
 
 
@@ -34,8 +235,7 @@ function add(){
 
 
 
-
-
+/*
 
 var display = document.getElementById('display')
 var minutos = document.getElementById('minutos')
@@ -84,4 +284,4 @@ comecar.addEventListener('click', function(){
 comecar.addEventListener('click', function(){
     comecar.innerHTML = "X"
 })
-
+*/
